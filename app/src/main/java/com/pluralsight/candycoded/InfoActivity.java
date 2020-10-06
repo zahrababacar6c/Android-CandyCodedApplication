8619c21b3e1931ff1ed5c9cee06433da683dcc43
zahrababacar6c <52515786+zahrababacar6c@users.noreply.github.com>
2020-10-06 12:38:37 +0100

    'module3'

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -28,6 +28,13 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent actionDialIntent= new Intent(Intent.ACTION_DIAL);
+        Uri telUri = Uri.parse("tel:0123456789");
+        actionDialIntent.setData(telUri);
+        startActivity(actionDialIntent);
+
+    }
     public void createMapIntent(View view){
         Uri mapUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
